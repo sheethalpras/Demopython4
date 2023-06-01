78bd72aa3bdaea740709b829968c9b3290a23954
sheethalpras <sheethalprash999@gmail.com>
2023-06-01 12:15:55 +0530

    added

---
+++ ecomproject/shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.import views
+
+app_name = 'shop'
+urlpatterns = [
+        path('', views.allProdcat, name='allProdcat'),
+        path('<slug:c_slug>/', views.allProdcat, name='products_by_category'),
+        path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name='prodCatdetail'),
+    ]
